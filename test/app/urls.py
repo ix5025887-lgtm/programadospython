3df0d7de77d352cb7604af2a8ad833376be7a7ca
ArthurAlder <95191406+ArthurAlder@users.noreply.github.com>
2026-02-09 11:36:13 -0300

    Delete test directory

--- test/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('produtos/', views.produtos, name='produtos'),
-    path('estoque/', views.estoque, name='estoque'),
-    path('vendas/', views.vendas, name='vendas'),
-]
